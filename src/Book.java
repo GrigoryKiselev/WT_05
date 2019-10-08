e2dc9a6fe26e272e93a8468e309a9c444ffd1d19
GrigoryKiselev <grigoriboshq@icloud.com>
2019-10-08 15:59:39 +0300

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,38 @@
+public class Book {
+    private Author author;
+    private String ISBN;
+    private int pages;
+    private String publishDate;
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+}
